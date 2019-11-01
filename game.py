55f697f712448ffac5b07dbc807a6e3b571fa37e
Morgan <e_morgan74403@ats-hr420-dm07.fc.ca>
2019-11-01 12:53:13 -0400

    lives added

--- game.py
+++ game.py
@@ -4,6 +4,10 @@ from random import randint
 #"basket" of choices
 choices=["rock", "paper", "scissors"]
 
+#adding lives, when one or the ther gets to 0, win / lose
+player_lives = 5
+compter_lives = 5
+
 #let the AI make choices
 computer=choices[randint(0,2)]
 
